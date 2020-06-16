be54b277e60a0ad78a49f81c5220c2c9a0619158
Pedro Alex L Martins <piedroalex@gmail.com>
2020-06-16 15:20:05 -0300

    Commit inicial com os arquivos do projeto.

---
+++ src/main/java/br/com/palm/pontointeligente/api/PontointeligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.palm.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/palm/pontointeligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.palm.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
