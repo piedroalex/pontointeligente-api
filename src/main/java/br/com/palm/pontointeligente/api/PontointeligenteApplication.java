44a45364601295328f6e97b513852c97f7815cd8
Pedro Alex L Martins <piedroalex@gmail.com>
2020-06-23 23:58:10 -0300

    Adicionando os controllers do cadastro de pessoa física e juridica.

--- src/main/java/br/com/palm/pontointeligente/api/PontointeligenteApplication.java
+++ src/main/java/br/com/palm/pontointeligente/api/PontointeligenteApplication.java
@@ -2,8 +2,9 @@ package br.com.palm.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PontointeligenteApplication {
 
 	public static void main(String[] args) {
