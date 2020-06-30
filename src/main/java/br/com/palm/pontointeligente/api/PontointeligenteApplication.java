e021249d84b7ea3dee14ff78e46d30ce5e79da5e
Pedro Alex L Martins <piedroalex@gmail.com>
2020-06-30 20:06:12 -0300

    Adicionando EhCache ao projeto.

--- src/main/java/br/com/palm/pontointeligente/api/PontointeligenteApplication.java
+++ src/main/java/br/com/palm/pontointeligente/api/PontointeligenteApplication.java
@@ -2,9 +2,10 @@ package br.com.palm.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
+@EnableCaching
 public class PontointeligenteApplication {
 
 	public static void main(String[] args) {
